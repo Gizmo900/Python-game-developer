12418960aa8b6e2f6b507c273f634ae2dee6b26a
Gizmo900 <jahromia75@gmail.com>
2024-08-31 18:03:33 +0300

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+#list
+colors = ["red","blue","yellow"]
+
+
+#dictionary
+#name is the key, "divya" is ther value
+details = {
+           "name":"divya",
+           "sex":"female",
+           "height":5.5
+           }
+
+print(details["height"]) #displays 5.5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
