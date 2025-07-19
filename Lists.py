7f5b4e298a05cde34d7700665931b36a98e9e377
Gizmo900 <jahromia75@gmail.com>
2025-07-19 12:01:45 +0100

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,19 @@
+"""variable
+"""
+num=10  
+#num is a variable name,= is used to assighn the value,10 is the value assigned
+
+#list
+num=[]   #empty list 
+numbers=[90,30,20,60,5]
+#to fetch any value from the list we consider the index number(position)the first index number is 0
+print(numbers[2])
+print(numbers[4])
+
+#iteration
+for i in range(len(numbers)):
+    print(i) #prints index number 
+    print(numbers[i]) #prints value placed at that location index number 
+
+#len function
+print(len(numbers)) #len function gives you number of elements present in the list
